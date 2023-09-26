4434cc0d7e09623107624261dbd5e237f561c07e
sergen uçar <sergenucar@sergens-MacBook-Pro.local>
2023-09-26 12:32:54 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import Message from './Message';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Message/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Message() {
+  return (
+    <div className="App">
+      <h1>Message</h1>
+    </div>
+  );
+}
+
+export default Message;
